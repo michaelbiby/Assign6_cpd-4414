05e1a8194aeb1adb77f3784b054dc3813a5925ed
michaelbiby <michaelbiby619@gmail.com>
2015-04-04 13:53:06 -0400

    Product_Listener Implemented

--- src/java/services/Prodct_listener.java
+++ src/java/services/Prodct_listener.java
@@ -3,13 +3,46 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package services;
+
+import entities.productlist;
+import entities.products;
+import java.io.StringReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.EJB;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+import javax.json.Json;
+import javax.json.JsonObject;
 
 /**
  *
  * @author c0644696
  */
-public class Prodct_listener {
-    
+@MessageDriven(mappedName = "jms/queue")
+public class Prodct_listener implements MessageListener {
+
+    @EJB
+    productlist productlist;
+
+    @Override
+    public void onMessage(Message message) {
+
+        try {
+            if (message instanceof TextMessage) {
+                String jsonString = ((TextMessage) message).getText();
+                JsonObject json = Json.createReader(new StringReader(jsonString)).readObject();
+                productlist.add(new products(json));
+            }
+        } catch (JMSException ex) {
+            System.err.println("Failure In JMS");
+        } catch (Exception ex) {
+            Logger.getLogger(Prodct_listener.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
 }
